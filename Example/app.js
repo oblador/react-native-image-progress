948899739811069d0636046c4070070ee0ab5b89
Joel Arvidsson <joel@oblador.se>
2016-09-26 09:24:21 +0200

    Update example to react-native 0.34 and es2016.

--- Example/app.js
+++ Example/app.js
@@ -1,95 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-
-var React = require('react');
-var ReactNative = require('react-native');
-var {
+import React, { Component } from 'react';
+import {
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
-  TouchableHighlight,
-  Platform,
-} = ReactNative;
+} from 'react-native';
 
+import Image from 'react-native-image-progress';
+import * as Progress from 'react-native-progress';
 
-var Image = require('react-native-image-progress');
+const INDICATORS = [null, Progress.Bar, Progress.Circle, Progress.Pie];
 
-var IMAGES = [
+const IMAGES = [
   'http://www.savethecat.com/wp-content/uploads/2015/06/cats.jpg',
   'http://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg',
   'http://media4.popsugar-assets.com/files/2014/08/08/878/n/1922507/caef16ec354ca23b_thumb_temp_cover_file32304521407524949.xxxlarge/i/Funny-Cat-GIFs.jpg',
-  'http://breadedcat.com/wp-content/uploads/2012/02/cat-breading-tutorial-004.jpg',
   'http://media1.santabanta.com/full1/Animals/Cats/cats-87a.jpg',
   'http://awesomegifs.com/wp-content/uploads/cat-smacks-at-hands.gif',
 ];
 
-var INDICATORS;
-if(Platform.OS === 'ios') {
-  var Progress = require('react-native-progress');
-  INDICATORS = [null, Progress.Bar, Progress.Circle, Progress.Pie];
-} else {
-  var ProgressBar = require('react-native-progress/Bar').default;
-  INDICATORS = [null, ProgressBar];
-}
-
-var Example = React.createClass({
-  getInitialState: function() {
-    return this.getRandomState();
-  },
-
-  getRandomState: function() {
-    return {
-      imageUri: this.getRandomImage(),
-      indicator: this.getRandomIndicator(),
-    };
-  },
-
-  getRandomIndicator: function() {
-    return INDICATORS.slice(0).sort( () => { return 0.5 - Math.random() } )[0];
-  },
-
-  getRandomImage: function() {
-    var images = IMAGES.slice(0);
-
-    //Filter out the current one since we don't want to see that one again
-    if(this.state && typeof this.state.image === 'object') {
-      images = images.filter((uri) => this.state.imageUri.indexOf(uri) !== 0 );
-    }
-    return images.sort( () => { return 0.5 - Math.random() } )[0] + '?rand=' + Date.now();
-  },
-
-  randomize: function() {
-    this.setState({ clear: true });
-    setTimeout(() => {
-      this.setState(this.getRandomState());
-      this.setState({ clear: false });
-    });
-  },
-
-  render: function() {
-    // React Native progress handlers are not reset when an image download is cancelled
-    // so to avoid jittering we'll force RN to use a new Image element for each source
-    if(this.state.clear) { return false; }
-
-    return (
-      <View style={styles.container}>
-        <TouchableHighlight onPress={this.randomize}>
-          <Image
-            source={{ uri: this.state.imageUri }}
-            indicator={this.state.indicator}
-            style={styles.image}
-            onLoaded={() => console.log('Image was loaded!')}
-          />
-        </TouchableHighlight>
-      </View>
-    );
-  }
-});
-
-var styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -99,7 +29,53 @@ var styles = StyleSheet.create({
   image: {
     width: 300,
     height: 200,
-  }
+  },
 });
 
-module.exports = Example;
+export default class Example extends Component {
+  constructor(props) {
+    super(props);
+    this.state = this.getRandomState();
+  }
+
+  getRandomState() {
+    return {
+      imageUri: this.getRandomImage(),
+      indicator: this.getRandomIndicator(),
+    };
+  }
+
+  getRandomIndicator() {
+    return INDICATORS.slice(0).sort( () => { return 0.5 - Math.random(); } )[0];
+  }
+
+  getRandomImage() {
+    let images = IMAGES.slice(0);
+
+    // Filter out the current one since we don't want to see that one again
+    if (this.state && typeof this.state.image === 'object') {
+      images = images.filter((uri) => this.state.imageUri.indexOf(uri) !== 0 );
+    }
+    return images.sort( () => { return 0.5 - Math.random(); } )[0] + '?rand=' + Date.now();
+  }
+
+  randomize = () => {
+    this.setState(this.getRandomState());
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity onPress={this.randomize}>
+          <Image
+            key={this.state.imageUri}
+            source={{ uri: this.state.imageUri }}
+            indicator={this.state.indicator}
+            style={styles.image}
+            onLoaded={() => console.log('Image was loaded!')}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
